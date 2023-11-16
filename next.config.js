4ff8c9db1fbc6ced80b0dcf02ecdbd11062e0735
goodluck04 <goodluckkumar04@gmail.com>
2023-11-16 10:44:10 +0530

    added core options

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images:{
-        domains:[
-            "images.pexels.com",
-            // another links for external images domain name for security reasons
-
-        ]
-    }
-}
-
-module.exports = nextConfig
+module.exports = {
+    async headers() {
+      return [
+        {
+          // matching all API routes
+          source: "/api/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
+    },
+    images: {
+      domains: [
+        "images.pexels.com",
+        // another links for external images domain name for security reasons
+      ],
+    },
+  };
+  
