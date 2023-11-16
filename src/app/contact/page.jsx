b8e0de11636423496020c06ec6e0d77c391a4e65
goodluck04 <goodluckkumar04@gmail.com>
2023-11-16 10:48:14 +0530

    final commit

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import styles from './page.module.css'
-import Image from 'next/image'
-import Button from '@/components/button/Button'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/button/Button";
 
 export const metadata = {
   title: "Info",
   description: "Cantact me",
 };
-
-
 
 const Contact = () => {
   return (
@@ -16,22 +14,28 @@ const Contact = () => {
       <h1 className={styles.title}>Let &apos; s Keep in Touch</h1>
       <dir className={styles.content}>
         <div className={styles.imgContainer}>
-          <Image 
+          <Image
             src="/contact.png"
-            alt='contact'
+            alt="contact"
             fill={true}
             className={styles.image}
           />
         </div>
         <form action="" className={styles.form}>
-          <input type="text" placeholder='name' className={styles.input} />
-          <input type="email" placeholder='email' className={styles.input} />
-          <textarea type="textArea" placeholder='message' cols="30" rows="10" className={styles.textArea} />
-        <Button url="#" text="Send" />
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="email" placeholder="email" className={styles.input} />
+          <textarea
+            type="textArea"
+            placeholder="message"
+            cols="30"
+            rows="10"
+            className={styles.textArea}
+          />
+          <Button url="mailto:goodluckkumar04@gmail.com" text="Send" />
         </form>
       </dir>
-    </div> 
-  )
-}
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
